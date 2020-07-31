b5251637869986dc417fa8445288ebd8f9f5a00b
leo <leo.kalshteyn@gmail.com>
2020-07-31 15:55:26 -0700

    adds dispatch functionality to the functions axios to actions

--- App.js
+++ App.js
@@ -1,16 +1,11 @@
 import React, { Component } from 'react';
 
-import { Provider } from 'react-redux';
-
 import Router from './app/modules/home/index'
-// import store from './app/redux/store';
 
 export default class App extends Component {
     render() {
         return (
-            /*<Provider store={store}>*/
             <Router />
-            // </Provider>
         );
     }
 }
--- app/modules/home/actions.js
+++ app/modules/home/actions.js
@@ -1,3 +1,4 @@
+import axios from 'axios';
 
 import * as retrieve from './constants';
 
@@ -5,11 +6,30 @@ export function getNewsHeadlines(country = "us") {
     return (dispatch) => {
         dispatch({type: retrieve.RETRIEVING_HEADLINES});
         return new Promise((resolve, reject) => {
+            const url = `${retrieve.API_URL}country=${country}&apiKey=${retrieve.API_KEY}&pageSize=20`;
+            axios.get(url)
+                .then(res => res.data)
+                .then((data) => {
+                    dispatch({type: retrieve.HEADLINES_AVAILABLE, data})
+                    resolve()
+                })
+                .catch(error => {
+                    dispatch({type: retrieve.HEADLINES_ERROR, error})
+                    reject()
+                });
         })
     };
 }
 
 export function getHeadlinesBySource(source) {
     return (dispatch) => {
+        dispatch({type: retrieve.RETRIEVING_HEADLINES});
+        return new Promise((resolve, reject) => {
+            const url = `${retrieve.API_URL}sources=${source}&apiKey=${retrieve.API_KEY}&pageSize=20`;
+                axios.get(url)
+                .then(res => res.data)
+                .then((data) => resolve(data))
+                .catch(error => reject(error));
+        })
     };
 }
