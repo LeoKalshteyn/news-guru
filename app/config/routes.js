2e3a72f90fa243f2c7c446fc22e5afafeefcdf39
leo <leo.kalshteyn@gmail.com>
2020-08-09 12:07:10 -0700

    saved edit

--- app/config/routes.js
+++ app/config/routes.js
@@ -1,52 +1,27 @@
 import React from 'react';
-import { View, StyleSheet, Text } from 'react-native';
 import { AppLoading } from 'expo';
 
 import { Scene, Router, Stack } from 'react-native-router-flux';
 
+import Home from '../modules/home/scenes/Home';
+import Article from '../modules/home/scenes/Article';
+import Source from '../modules/home/scenes/Source';
+
+import { color, navTitleStyle } from "../styles/theme";
+
 export default class extends React.Component {
-    state = {
-        working: false
-    };
-
-    componentDidMount() {
-        setTimeout(()=> this.setState({working: true}), 1000)
-    }
-
     render() {
-        let navTitleStyle = { fontSize: 10 , fontFamily: "Times New Roman", letterSpacing: 0.7 };
-
-        if (!this.state.working)
-            return <AppLoading/>
-
         return (
             <Router>
                 <Stack key="root"
-                       navigationBarStyle={{backgroundColor: "#f33"}}
+                       navigationBarStyle={{backgroundColor: "#fff"}}
                        titleStyle={navTitleStyle}
-                       backButtonTintColor={'#1E1611'}>
-                    <Scene key="Main" component={Main} title="Main" initial/>
+                       backButtonTintColor={color.black}>
+                    <Scene key="Home" component={Home} title="Headlines" initial/>
+                    <Scene key="Article" component={Article} title=""/>
+                    <Scene key="Source" component={Source} title=""/>
                 </Stack>
             </Router>
         )
     }
 }
-
-class Main extends React.Component {
-    render() {
-        return (
-            <View style={styles.container}>
-                <Text>Test</Text>
-            </View>
-        );
-    }
-}
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#300',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-});
